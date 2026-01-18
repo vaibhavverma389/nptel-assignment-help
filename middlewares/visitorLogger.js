2d9da2b4f9b4c8a452ad9d27ebf9e9baff61db51
vaibhavverma389 <vaibhavverma6393@gmail.com>
2026-01-18 21:55:00 +0530

    next

--- middlewares/visitorLogger.js
+++ middlewares/visitorLogger.js
@@ -1,5 +1,5 @@
 const VisitorLog = require("../models/VisitorLog");
-const geoip = require("geoip-lite");
+const axios = require("axios");
 
 module.exports = async (req, res, next) => {
   try {
@@ -7,13 +7,24 @@ module.exports = async (req, res, next) => {
       req.headers["x-forwarded-for"]?.split(",")[0] ||
       req.socket.remoteAddress;
 
-    const geo = geoip.lookup(ip) || {};
+    let isp = "Unknown";
+    let asn = "Unknown";
+
+    // Skip localhost
+    if (ip && ip !== "::1" && ip !== "127.0.0.1") {
+      const response = await axios.get(`https://ipwho.is/${ip}`);
+
+      if (response.data && response.data.success) {
+        isp = response.data.isp || "Unknown";
+        asn = response.data.asn || "Unknown";
+      }
+    }
 
     await VisitorLog.create({
       ip,
-      email: req.user ? req.user.email : null,   // 🔥 EMAIL HERE
-      isp: geo.org || "Unknown",
-      asn: geo.asn || "Unknown",
+      email: req.user ? req.user.email : null,
+      isp,
+      asn,
       userAgent: req.headers["user-agent"],
       path: req.originalUrl
     });
