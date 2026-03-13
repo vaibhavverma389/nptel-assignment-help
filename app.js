875d0802e6dfa3cfaf3eea8fd121e56d4f1250f0
vaibhavverma389 <vaibhavverma6393@gmail.com>
2026-03-13 22:04:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -147,6 +147,6 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 3000;
 
-// server.listen(PORT, () => {
-//   console.log(`🚀 Server running on http://localhost:${PORT}`);
-// });
+server.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
