4735fe7781cc9b3907256cb9f928a9f9e85f4f82
vaibhavverma389 <vaibhavverma6393@gmail.com>
2026-04-10 20:46:55 +0530

    Update studentRoutes.js

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -24,7 +24,7 @@ const isValidWeek = (week) => {
 };
 
 /* ================= DASHBOARD ================= */
-router.get("/dashboard", asyncHandler(async (req, res) => {
+router.get("/dashboard",isAuth, asyncHandler(async (req, res) => {
   const { course = "", week = null } = req.query;
   const weekNum = week ? Number(week) : null;
 
